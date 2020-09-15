59057468893b31f6c90a8b89a0a236e67520be1d
Donione <ondrej.adamec.cze@gmail.com>
2020-09-15 20:50:40 +0200

    Handle window messages using Window class member function

--- Hedgehog/Include/Window/Window.h
+++ Hedgehog/Include/Window/Window.h
@@ -2,17 +2,50 @@
 
 #include <Windows.h>
 
+#include <string>
+#include <functional>
+
 
 class Window
 {
 public:
-    Window(): hwnd(NULL) {}
+    struct WindowProperties
+    {
+        std::string title;
+        unsigned int width;
+        unsigned int height;
 
-	void Create(HINSTANCE hInstance, const wchar_t WINDOW_NAME[]);
+        WindowProperties(const std::string& title = "Hedgehog Window",
+                         unsigned int width = 1280,
+                         unsigned int height = 720)
+            :
+            title(title),
+            width(width),
+            height(height)
+        {
+        }
+    };
+
+public:
+    Window() : hwnd(NULL) {}
+
+	void Create(HINSTANCE hInstance, const WindowProperties windowProperties = WindowProperties());
+    void SetMessageCallback(const std::function<void(void)> callback);
 	void Show(int nShowCmd = SW_SHOW);
 
 private:
+    LRESULT HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+    static LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+private:
 	HWND hwnd = NULL;
+
+    std::string title = "";
+    unsigned int width = 0;
+    unsigned int height = 0;
+
+    std::function<void(void)> MessageCallback = NULL;
 };
 
 
--- Hedgehog/Source/Window/Window.cpp
+++ Hedgehog/Source/Window/Window.cpp
@@ -4,41 +4,89 @@
 #include <cstdio>
 
 
-static LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+LRESULT Window::HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
     switch (uMsg)
     {
+    case WM_CREATE:
+        printf("%s: Window created.\n", title.c_str());
+        return DefWindowProc(hwnd, uMsg, wParam, lParam);
+
+    case WM_CLOSE:
+        printf("%s: Clicked close.\n", title.c_str());
+        return DefWindowProc(hwnd, uMsg, wParam, lParam);
+
     case WM_DESTROY:
+        printf("%s: Window destroyed.\n", title.c_str());
         PostQuitMessage(0);
         return 0;
 
-    case WM_PAINT:
+    case WM_LBUTTONDBLCLK:
+        if (MessageCallback)
         {
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hwnd, &ps);
-
-            FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
-
-            EndPaint(hwnd, &ps);
+            MessageCallback();
         }
+        return DefWindowProc(hwnd, uMsg, wParam, lParam);
+
+    case WM_PAINT:
+    {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hwnd, &ps);
+
+        FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
+
+        EndPaint(hwnd, &ps);
         return 0;
+    }
 
     default:
         return DefWindowProc(hwnd, uMsg, wParam, lParam);
     }
 }
 
-void Window::Create(HINSTANCE hInstance, const wchar_t WINDOW_NAME[])
+LRESULT CALLBACK Window::WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
+    Window* pThis = NULL;
+
+    if (uMsg == WM_CREATE)
+    {
+        CREATESTRUCT* pCreate = reinterpret_cast<CREATESTRUCT*>(lParam);
+        pThis = reinterpret_cast<Window*>(pCreate->lpCreateParams);
+        SetWindowLongPtr(hwnd, GWLP_USERDATA, (LONG_PTR)pThis);
+    }
+    else
+    {
+        LONG_PTR ptr = GetWindowLongPtr(hwnd, GWLP_USERDATA);
+        pThis = reinterpret_cast<Window*>(ptr);
+    }
+
+    if (pThis)
+    {
+        return pThis->HandleMessage(uMsg, wParam, lParam);
+    }
+    else
+    {
+        return DefWindowProc(hwnd, uMsg, wParam, lParam);
+    }
+}
+
+void Window::Create(HINSTANCE hInstance, const WindowProperties windowProperties)
+{
+    title = windowProperties.title;
+    width = windowProperties.width;
+    height = windowProperties.height;
+
+    // MessageCallback = ;
+
 	// Fill the window class
 	// Note that this is not a C++ "class" but rather an internal structure used by the operating system
-	const wchar_t CLASS_NAME[] = L"Sample Window Class";
+	const wchar_t CLASS_NAME[] = L"Hedgehog Window Class";
 
 	WNDCLASS wc = { };
-
 	wc.lpfnWndProc = WindowProc;
 	wc.hInstance = hInstance;
 	wc.lpszClassName = CLASS_NAME;
+    wc.style |= CS_DBLCLKS;
 
 	// Register the window class with the operating system
 	RegisterClass(&wc);
@@ -46,17 +94,25 @@ void Window::Create(HINSTANCE hInstance, const wchar_t WINDOW_NAME[])
     hwnd = CreateWindowEx(
         0,                              // Optional window styles.
         CLASS_NAME,                     // Window class
-        WINDOW_NAME,    // Window text
+        std::wstring(windowProperties.title.begin(), windowProperties.title.end()).c_str(),    // Window text
         WS_OVERLAPPEDWINDOW,            // Window style
 
         // Size and position
-        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+        CW_USEDEFAULT, // X 
+        CW_USEDEFAULT, // Y
+        windowProperties.width, // Width
+        windowProperties.height, // Height
 
         NULL,       // Parent window    
         NULL,       // Menu
         hInstance,  // Instance handle
-        NULL        // Additional application data
+        this        // Additional application data
     );
+}
+
+void Window::SetMessageCallback(const std::function<void(void)> callback)
+{
+    MessageCallback = callback;
 }
 
 void Window::Show(int nShowCmd)
