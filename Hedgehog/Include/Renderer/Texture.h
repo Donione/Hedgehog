30810c949d503d06ec9de7c450853c65edd12e15
Donione <ondrej.adamec.cze@gmail.com>
2021-01-12 15:35:54 +0100

    Create simple abstract Texture class

---
+++ Hedgehog/Include/Renderer/Texture.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <string>
+
+
+class Texture
+{
+public:
+	virtual ~Texture() = default;
+
+	virtual void Bind(unsigned int slot = 0) const = 0;
+
+	virtual unsigned int GetWidth() const = 0;
+	virtual unsigned int GetHeight() const = 0;
+};
+
+
+class Texture2D : public Texture
+{
+public:
+	static Texture2D* Create(const std::string& filename);
+};
