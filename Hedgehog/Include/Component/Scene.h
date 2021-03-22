795a7b26b5c9926dc2d37ae4e76867907e6e80e7
Donione <ondrej.adamec.cze@gmail.com>
2021-03-22 14:19:35 +0100

    Crate scene class which wraps EnTT's registry and handles Entities and scene actions

---
+++ Hedgehog/Include/Component/Scene.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <Component/Entity.h>
+#include <entt.hpp>
+
+#include <Renderer/Camera.h>
+
+
+namespace Hedge
+{
+
+class Scene
+{
+public:
+	Scene();
+
+	Entity CreateEntity(const std::string& name);
+
+	void DestroyEntity(Entity entity);
+
+	void OnUpdate();
+
+	void UpdateRenderSettings();
+
+public:
+	int plUsed = 3;
+
+	Camera* camera = nullptr;
+
+
+public:
+	entt::registry registry;
+};
+
+} // namespace Hedge
