326c184211cfe1a20c0203afc30d272244fe3fc6
Donione <ondrej.adamec.cze@gmail.com>
2021-04-07 12:19:49 +0200

    Create a shader that uses normal mapping (based previous model shader)

---
+++ Hedgehog/Asset/Shader/OpenGLNormalMapPixelShader.glsl
@@ -0,0 +1,187 @@
+#version 460 core
+
+struct DirectionalLight
+{
+    vec3 color;
+    vec3 direction;
+};
+
+struct PointLight
+{
+    vec3 color;
+    vec3 position;
+    vec3 attenuation;// x = constant, y = linear, z = quadratic components
+};
+
+struct SpotLight
+{
+   vec3 color;
+   vec3 position;
+   vec3 attenuation; // x = constant, y = linear, z = quadratic components
+   vec3 direction;
+   vec2 cutoffAngle;
+};
+
+layout(location = 0) out vec4 a_color;
+
+in vec3 v_Position;
+in vec2 v_textureCoordinates;
+in mat3 TBN;
+in vec3 v_positionTan;
+in vec3 v_lightPosTan;
+in vec3 v_viewPosTan;
+in vec3 v_normalTan;
+
+uniform vec3 u_viewPos;
+uniform DirectionalLight u_directionalLight;
+uniform int u_numberOfPointLights;
+uniform PointLight u_pointLight[3];
+uniform SpotLight u_spotLight;
+
+uniform bool u_normalMapping;
+uniform float u_specularStrength;
+
+uniform sampler2D t_texture;
+uniform sampler2D t_normalMap;
+//uniform sampler2D t_specularMap;
+
+
+vec3 CalculateDirectionalLight(vec3 objectColor,
+                               vec3 lightDirection, // normalized direction from pixel to the light
+                               vec3 lightColor,
+                               vec3 position,       // pixel position
+                               vec3 normal)         // normalized pixel normal vector
+{
+    if (lightDirection.z < 0)
+    {
+        return vec3(0.0f, 0.0f, 0.0f);
+    }
+
+    vec3 ambient = vec3(0.0f, 0.0f, 0.0f);
+    
+    float diff = max(dot(normal, lightDirection), 0.0f);
+    vec3 diffuse = diff * lightColor;
+
+    float specularStrength = 0.2f;
+    //vec3 viewDirection = normalize(u_viewPos - position);
+    //vec3 viewDirection = TBN * normalize(u_viewPos - position);
+    vec3 viewDirection = normalize(v_viewPosTan - position);
+    vec3 reflectDirection = reflect(-lightDirection, normal);
+    float spec = pow(max(dot(viewDirection, reflectDirection), 0.0), 32);
+
+    vec3 specular = u_specularStrength * spec * lightColor;// * texture(u_specularMap, v_textureCoordinates).rgb;
+
+    return (ambient + diffuse + specular) * objectColor;
+}
+
+vec3 CalculatePointLight(vec3 objectColor,
+                         vec3 lightPosition,
+                         vec3 lightColor,
+                         vec3 attenuation,   // vector of attenuation components, x = constant, y = linear, z = quadratic
+                         vec3 position,      // pixel position
+                         vec3 normal)
+{
+    vec3 lightDirection = normalize(lightPosition - position);
+    //vec3 lightDirection = TBN * normalize(lightPosition - position);
+
+    vec3 result = CalculateDirectionalLight(objectColor, lightDirection, lightColor, position, normal);
+
+    // Attenuation is computed according to the formula:
+    //    1 / ( Kc + d * Kl + d^2 * Kq ), where
+    //        d = distance
+    //       Kc = constant component
+    //       Kl = linear component
+    //       Kq = quadratic component
+    float lightDistance = length(lightPosition - position);
+    float att = 1.0f / (attenuation.x + lightDistance * attenuation.y + lightDistance * lightDistance * attenuation.z);
+
+    return att * result;
+}
+
+vec3 CalculateSpotLight(vec3 objectColor,
+                        vec3 lightPosition,
+                        vec3 lightColor,
+                        vec3 lightDirection, // normalized direction into the spotlight in world space
+                        vec2 cutoffAngle,    //cosine of angles from lightDirection to the inner and outer cutoff radius in radians
+                        vec3 attenuation,    // vector of attenuation components, x = constant, y = linear, z = quadratic
+                        vec3 position,       // pixel position
+                        vec3 normal)         // normalized pixel normal vector
+{
+    vec3 result;
+
+    vec3 lightDir = normalize(lightPosition - position);
+    // The angle between two vectors can be computed using the following formula:
+    //    cos theta = dot(u, v) / (length(u) * length(v))
+    // Since both u and v are normalized (their length is 1), the formula can be simplified to:
+    //    cos theta = dot(u, v),
+    // and then
+    //    theta = acos(dot(u, v))
+    //float theta = acos(dot(lightDir, lightDirection));
+
+    // For soft edges, we compute the light intensity using the following formula:
+    //    I = (theta - outer radius) / (outer radius - inner radius)
+    // and then limiting the Intensity between 0.0 and 1.0
+    // Basically we're computing the ratio between theta and the soft edge area,
+    //float epsilon = cutoffAngle.y - cutoffAngle.x;
+    //float intensity = smoothstep(0.0f, 1.0f, (cutoffAngle.y - theta) / epsilon);
+
+    // To go without the acos here in the shader, we can input the angles already in the form of cosine of the angle
+    // and tweak the calculations as follows:
+    float cosTheta = dot(lightDir, lightDirection);
+    float epsilon = cutoffAngle.x - cutoffAngle.y;
+    float intensity = smoothstep(0.0f, 1.0f, (cosTheta - cutoffAngle.y) / epsilon);
+
+    result = intensity * CalculatePointLight(objectColor, lightPosition, lightColor, attenuation, position, normal);
+
+    return result;
+}
+
+
+
+void main()
+{
+    vec3 result = vec3(0.0f, 0.0f, 0.0f);
+
+    vec3 objectColor = texture(t_texture, v_textureCoordinates).rgb;
+
+    vec3 normal;
+    if (u_normalMapping)
+    {
+    // obtain normal from normal map in range [0,1]
+    normal = texture(t_normalMap, v_textureCoordinates).rgb;
+    // transform normal vector to range [-1,1]
+    normal = normal * 2.0f - 1.0f;
+    // transform normal sample from tangent space to world space
+    //normal = normalize(TBN * normal);
+    normal = normalize(normal);
+    }
+    else
+    {
+        //normal = normalize(v_Normal);
+        normal = normalize(v_normalTan);
+    }
+
+    result += CalculateDirectionalLight(objectColor,
+                                        normalize(TBN * -u_directionalLight.direction),
+                                        u_directionalLight.color,
+                                        v_positionTan,
+                                        normal);
+
+    for (int i = 0; i < u_numberOfPointLights; i++)
+    {
+        //result += CalculatePointLight(objectColor, u_pointLight[i].position, u_pointLight[i].color, u_pointLight[i].attenuation, v_Position, normal);
+        result += CalculatePointLight(objectColor, v_lightPosTan, u_pointLight[i].color, u_pointLight[i].attenuation, v_positionTan, normal);
+    }
+
+    result += CalculateSpotLight(objectColor,
+                                 u_spotLight.position,
+                                 //TBN * u_spotLight.position,
+                                 u_spotLight.color,
+                                 normalize(TBN * -u_spotLight.direction),
+                                 u_spotLight.cutoffAngle,
+                                 u_spotLight.attenuation,
+                                 v_positionTan,
+                                 normal);
+
+    a_color = vec4(result, 1.0f);
+}
---
+++ Hedgehog/Asset/Shader/OpenGLNormalMapVertexShader.glsl
@@ -0,0 +1,55 @@
+#version 460 core
+
+struct PointLight
+{
+    vec3 color;
+    vec3 position;
+    vec3 attenuation;// x = constant, y = linear, z = quadratic components
+};
+
+layout(location = 0) in vec3 a_position;
+layout(location = 1) in vec3 a_normal;
+layout(location = 2) in vec2 a_textureCoordinates;
+layout(location = 3) in vec3 a_tangent;
+layout(location = 4) in vec3 a_bitangent;
+
+uniform mat4 u_ViewProjection;
+uniform mat4 u_Transform;
+
+uniform vec3 u_viewPos;
+uniform PointLight u_pointLight[3];
+
+out vec3 v_Position;
+out vec2 v_textureCoordinates;
+out mat3 TBN;
+out vec3 v_positionTan;
+out vec3 v_lightPosTan;
+out vec3 v_viewPosTan;
+out vec3 v_normalTan;
+
+void main()
+{
+	vec4 position = u_Transform * vec4(a_position, 1.0f);
+
+	gl_Position = u_ViewProjection * position;
+	
+	v_Position = vec3(position);
+	v_textureCoordinates = a_textureCoordinates;
+
+
+	vec3 T = normalize(vec3(u_Transform * vec4(a_tangent,   0.0)));
+	vec3 B = normalize(vec3(u_Transform * vec4(a_bitangent, 0.0)));
+	vec3 N = normalize(vec3(u_Transform * vec4(a_normal,    0.0)));
+	
+	// pass the TBN matrix to pixel shader to transform normal samples to world space
+	TBN = mat3(T, B, N);
+
+	// or invert it before passing to insted transform light vectors into tangent space
+	TBN = transpose(TBN);
+
+	// or transform all the relevant light vectors to tangent space here and pass those
+	v_positionTan = TBN * v_Position;
+	v_lightPosTan = TBN * u_pointLight[0].position;
+	v_viewPosTan = TBN * u_viewPos;
+	v_normalTan = TBN * vec3(u_Transform * vec4(a_normal, 0.0f));
+}
